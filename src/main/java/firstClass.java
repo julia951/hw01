1e2986fa086cfff9c588cabfaa29211148a1dc7b
julia951 <klimuk.julia@gmail.com>
2022-05-29 00:01:27 +0300

    third commit

--- src/main/java/firstClass.java
+++ src/main/java/firstClass.java
@@ -2,6 +2,6 @@ public class firstClass {
 
     public static void main(String[] args) {
         System.out.println("Hi QAGuru!!!020202");
-        System.out.println("Hi from new Branch");
+        System.out.println("Hi from new Branch!!!!!!");
     }
 }
